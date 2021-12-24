cdde1f7e1c2b381955d56c6c49e042e47221cf45
Kamonphet <62428925+Kamonphet@users.noreply.github.com>
2021-12-24 08:59:52 +0700

    Add files via upload

---
+++ js/zoom.js
@@ -0,0 +1,14 @@
+mediumZoom('.zoom',{
+    margin:50,
+    background: '#e3c1f7',
+})
+
+mediumZoom('.zoomprofile',{
+    margin:50,
+    background: '#fddbfa',
+})
+
+mediumZoom('.zoomhome',{
+    margin:50,
+    background: '#d5c6f4',
+})
