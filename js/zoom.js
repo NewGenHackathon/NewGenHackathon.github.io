ea629f6de0735c147799f65d0c79ef0a43256b7f
Kamonphet <62428925+Kamonphet@users.noreply.github.com>
2021-12-27 14:44:09 +0700

    Add files via upload

--- js/zoom.js
+++ js/zoom.js
@@ -1,14 +1,5 @@
 mediumZoom('.zoom',{
     margin:50,
-    background: '#e3c1f7',
-})
-
-mediumZoom('.zoomprofile',{
-    margin:50,
-    background: '#fddbfa',
-})
-
-mediumZoom('.zoomhome',{
-    margin:50,
-    background: '#d5c6f4',
+    scrollOffset: 0,
+    background: 'rgba(25, 18, 25, .9)'
 })
